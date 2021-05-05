8a76751e99e3902e480fba37511c3dbbce59655d
Dr. Rubisco <76263371+thedocruby@users.noreply.github.com>
2021-05-05 10:03:17 -0400

    Update and rename src/main/java/net/fabricmc/example/ExampleMod.java to src/main/java/net/thedocruby/mimicraft/MimiCraft.java
    more work un-exampling the example template

---
+++ src/main/java/net/thedocruby/mimicraft/MimiCraft.java
@@ -0,0 +1,10 @@
+package net.fabricmc.example;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MimiCraft implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		System.out.println("Hello Fabric world! This is MimiCraft!");
+	}
+}
